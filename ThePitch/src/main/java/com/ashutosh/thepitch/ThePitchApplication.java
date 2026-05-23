00ed73d16d1ce07df91a485f35b9c53d7c100d63
ashutosh <ashutoshmaurya7860@gmail.com>
2026-05-23 09:41:03 +0530

    Setting the Dependencies

---
+++ ThePitch/src/main/java/com/ashutosh/thepitch/ThePitchApplication.java
@@ -0,0 +1,13 @@
+package com.ashutosh.thepitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePitchApplication.class, args);
+	}
+
+}
